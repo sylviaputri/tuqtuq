34df1bb2515a920d59570676892db0c518f1ac89
sylviaputri <sylvi.bluemochi@gmail.com>
2018-09-26 23:58:16 +0700

    bikin project animatedball lagi (baru, ngulang lagi)

--- animatedball/animatedball/My Project/Resources.Designer.vb
+++ animatedball/animatedball/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property best_air_hockey_table() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("best-air-hockey-table", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
--- animatedball/animatedball/SelectArena.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class SelectArena
-
-End Class
